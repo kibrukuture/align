bbdd25e5229a0a85539992b1f560c177538951b7
Kibru Kuture <52848197+kibrukuture@users.noreply.github.com>
2025-11-20 12:06:00 +0300

    chore: remove unused VERIFY endpoint from wallets

--- src/constants/wallets.endpoints.ts
+++ src/constants/wallets.endpoints.ts
@@ -3,6 +3,5 @@
  */
 export const WALLET_ENDPOINTS = {
   /** Endpoint to verify wallet ownership */
-  VERIFY: '/wallets/verify',
   VERIFY_OWNERSHIP: (customerId: string) => `/v0/customers/${customerId}/wallet-ownership`,
 } as const;
